004f6eab74eaa45eee10e1ba5d5a80197180b9f9
AlburIvan <albur.ivan@outlook.com>
2017-12-05 02:00:52 -0400

    [WIP] Section 15

---
+++ App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableHighlight, 
+  Button
+} from "react-native";
+import firebase from "firebase";
+import Config from 'react-native-config';
+
+
+import { Header, Spinner } from "./src/components/common";
+import LoginForm from "./src/components/LoginForm";
+
+export default class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    console.log(Config);
+
+    firebase.initializeApp({
+      apiKey: Config.FIREBASE_API_KEY,
+      authDomain:  Config.FIREBASE_AUTH_DOMAIN,
+      databaseURL:  Config.FIREBASE_DATABSE_URL,
+      projectId:  Config.FIREBASE_PROJECT_ID,
+      storageBucket:  Config.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId:  Config.FIREBASE_SENDER_ID
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+
+    this._onLogoutPressed.bind(this)
+  }
+
+  _onLogoutPressed = () => {
+    firebase.auth().signOut();
+    this.setState({ loggedIn: false });
+  };
+
+  // _renderContent = () => {
+  //   switch (this.state.loggedIn) {
+  //     case true:
+  //       return (
+  //           <View style={styles.signinField}>
+  //             <TouchableHighlight onPress={this._onLogoutPressed} underlayColor="white">
+  //                 <View style={styles.signinButton}>
+  //                     <Text style={styles.buttonText}>
+  //                         {'Sign out'.toUpperCase()}
+  //                     </Text>
+  //                 </View>
+  //             </TouchableHighlight>
+  //           </View>
+  //       );
+
+  //     case false:
+  //       return <LoginForm />;
+
+  //     default:
+  //         <Spinner size="large" />
+  //   }
+  // };
+
+  render() {
+    return <LoginForm />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  signinField: {
+    marginTop: 15,
+    marginLeft: 35,
+    marginRight: 35,
+  },
+  signinButton: {
+    alignItems: 'center',
+    borderRadius: 45,
+    backgroundColor: '#4ed589',
+  },
+  buttonText: {
+    fontSize: 16,
+    padding: 15,
+    color: 'white'
+  },
+});
